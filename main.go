cf083bbef8a90f7596107edfa67314cdaab2130f
Ulises Flynn <uflynn@hotmail.com>
2018-11-06 02:08:55 -0700

    cleanup code, and update dep

--- main.go
+++ main.go
@@ -3,83 +3,90 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"time"
 
-	"github.com/gota/dataframe"
+	"github.com/kniren/gota/dataframe"
 )
 
-// JSONMap is a map string interface for querried data
-type JSONMap map[string]interface{}
-
-var (
-	// Key the api key needed to make requests to government supported APIs.
-	Key = os.Getenv("API_KEY")
-	// Maps are a slice of all maps
-	Maps []map[string]interface{}
-)
-
-// GetData querries all gov apis for data.
-func GetData(client http.Client, url, headerKey, header string) (df dataframe.DataFrame, err error) {
+// GetData makes an http call with the given url to get a dataframe.
+func GetData(client http.Client, apiKey string, maps []map[string]interface{}, url, headerKey, header string) (df dataframe.DataFrame, err error) {
+	// create request
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		log.Println("unable to create request on url: ", url, err)
+		return df, fmt.Errorf("unable to create request on url: %s, error: %s", url, err)
 	}
 
-	req.Header.Add("X-Api-Key", Key)
+	// add api key header
+	req.Header.Add("X-Api-Key", apiKey)
 	if headerKey != "" {
 		req.Header.Add("Accept", "application/vnd.api+json")
 	}
 
+	// perform request
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Println("unable to perform request on url:", url, err)
-		return df, err
+		return df, fmt.Errorf("unable to perform request on url: %s, error: %s", url, err)
 	}
 	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		log.Println("unable to reach ", url, resp.Status)
-		return df, err
-	}
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Println("unable to read body from ", url, err)
-		return df, err
-	}
-	var m map[string]interface{}
-	err = json.Unmarshal(body, &m)
-	if err != nil {
-		log.Println("cannot Unmarshal, ", err)
-		return df, err
-	}
-	Maps := append(Maps, m)
-	fmt.Println(len(m))
 
-	df = dataframe.LoadMaps(Maps)
-	return df, nil
+	// check status code
+	if resp.StatusCode != http.StatusOK {
+		return df, fmt.Errorf("failure getting data from url: %s, status code: %s", url, resp.Status)
+	}
+
+	// json decode the response body
+	m := make(map[string]interface{})
+	err = json.NewDecoder(resp.Body).Decode(&m)
+	if err != nil {
+		return df, fmt.Errorf("unable to perform request on url: %s, error: %s", url, err)
+	}
+
+	// add result to maps
+	maps = append(maps, m)
+
+	// return dataframe from the maps
+	return dataframe.LoadMaps(maps), nil
 }
 
 func main() {
+	// get api key from environment
+	apiKey := os.Getenv("API_KEY")
+
+	// setup map to hold dataframe results
+	maps := make([]map[string]interface{}, 0)
+
+	// open file with list of urls
 	f, err := os.Open("url.csv")
 	if err != nil {
-		log.Fatal("unable to access csv ", err)
+		log.Fatalf("unable to access csv: %s", err)
+		return
 	}
+
+	// build dataframe from csv
 	df := dataframe.ReadCSV(f)
-	fmt.Println(df)
 	if df.Nrow() == 0 || df.Ncol() == 0 {
-		log.Fatal("your cvs is empty")
+		log.Fatal("your csv is empty")
+		return
 	}
+
+	// create client to get data
+	client := &http.Client{
+		Timeout: 30 * time.Second,
+	}
+
+	// process all the dataframes and get the data
 	for i := 0; i < df.Nrow(); i++ {
-		client := &http.Client{
-			Timeout: 30 * time.Second,
-		}
-		multidf, err := GetData(*client, df.Elem(i, 0).String(), df.Elem(i, 1).String(), df.Elem(i, 2).String())
+		url := df.Elem(i, 0).String()
+		headerKey := df.Elem(i, 1).String()
+		header := df.Elem(i, 2).String()
+		dataFrame, err := GetData(*client, apiKey, maps, url, headerKey, header)
 		if err != nil {
-			log.Fatal(err)
+			fmt.Printf("unable to get data for url: %s, error: %s", url, err)
+			continue
 		}
-		fmt.Println(multidf)
+		fmt.Println(dataFrame)
 	}
 }
